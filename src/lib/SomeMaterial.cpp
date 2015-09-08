74f969de17e9c81cca3fca18d83b939976404e0f
Maxim Fedorenko <varlllog@gmail.com>
2015-09-09 00:26:49 +0300

    [style] Astyle.

--- src/lib/SomeMaterial.cpp
+++ src/lib/SomeMaterial.cpp
@@ -1,18 +1,18 @@
-// 
+//
 // The MIT License (MIT)
 //
 // Copyright (c) 2015 Maxim Fedorenko <varlllog@gmail.com>
-// 
+//
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
 // in the Software without restriction, including without limitation the rights
 // to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 // copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
-// 
+//
 // The above copyright notice and this permission notice shall be included in all
 // copies or substantial portions of the Software.
-// 
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -20,7 +20,7 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
-// 
+//
 //  SomeMaterial.cpp
 //
 //  Created by Maxim Fedorenko <varlllog@gmail.com> on 08/Sep/2015.
@@ -31,51 +31,56 @@
 #include "iostream"
 
 #include <Eigen/Core>
-#include <Eigen/Geometry> 
+#include <Eigen/Geometry>
 
-namespace NanoMagnetic
-{
+namespace NanoMagnetic {
 
 SomeMaterial::SomeMaterial()
-    : mK1(0.1)
-    , mMs(0.1)
-    , mRadius(1)
-    , mDiametr(mRadius*2)
-    , mHk(2 * mK1 / mMs)
-    , mVolume(4 * M_PI * mRadius * mRadius * mRadius / 3)
+    : mK1( 0.1 )
+    , mMs( 0.1 )
+    , mRadius( 1 )
+    , mDiametr( mRadius * 2 )
+    , mHk( 2 * mK1 / mMs )
+    , mVolume( 4 * M_PI * mRadius * mRadius * mRadius / 3 )
 {
     Eigen::Matrix3f mat1;
     mat1 << 1, 2, 3,
-            4, 5, 6,
-            7, 8, 9;
+         4, 5, 6,
+         7, 8, 9;
 
     auto mat2 = mat1.transpose();
 
     std::cout << "mat2: " << std::endl << mat2 << std::endl;
 }
 
-double SomeMaterial::K1() const {
-  return mK1;
+double SomeMaterial::K1() const
+{
+    return mK1;
 }
 
-double SomeMaterial::Ms() const {
-  return mMs;
+double SomeMaterial::Ms() const
+{
+    return mMs;
 }
 
-double SomeMaterial::Hk() const {
-  return mHk;
+double SomeMaterial::Hk() const
+{
+    return mHk;
 }
 
-double SomeMaterial::Radius() const {
-  return mRadius;
+double SomeMaterial::Radius() const
+{
+    return mRadius;
 }
 
-double SomeMaterial::Diametr() const {
-  return mDiametr;
+double SomeMaterial::Diametr() const
+{
+    return mDiametr;
 }
 
-double SomeMaterial::Volume() const {
-  return mVolume;
+double SomeMaterial::Volume() const
+{
+    return mVolume;
 }
 
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,18 @@
-// 
+//
 // The MIT License (MIT)
 //
 // Copyright (c) 2015 Maxim Fedorenko <varlllog@gmail.com>
-// 
+//
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
 // in the Software without restriction, including without limitation the rights
 // to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 // copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
-// 
+//
 // The above copyright notice and this permission notice shall be included in all
 // copies or substantial portions of the Software.
-// 
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -20,7 +20,7 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
-// 
+//
 //  main.cpp
 //
 //  Created by Maxim Fedorenko <varlllog@gmail.com> on 08/Sep/2015.
@@ -47,6 +47,7 @@ void usage( const char *execName )
 int main( int argc, char *argv[] )
 {
     char c;
+
     while( ( c = getopt( argc, argv, "h" ) ) != -1 )
     {
         switch ( c )
@@ -57,6 +58,7 @@ int main( int argc, char *argv[] )
                 return EXIT_SUCCESS;
                 break;
             }
+
             default:
             {
                 usage( argv[0] );
@@ -65,12 +67,12 @@ int main( int argc, char *argv[] )
             }
         }
     }
-    
+
     int res = EXIT_SUCCESS;
 
     NanoMagnetic::SomeMaterial material;
     std::cout << "K1 == " << material.K1() << std::endl;
 
-    return res; 
+    return res;
 }
 
