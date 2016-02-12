aa4a783a0b9ab2ebc03f40ea6c62d3b34e80e8db
Maxim Fedorenko <Maxim.Fedorenko@texturacorp.com>
2016-02-12 18:36:51 +0300

    Start over.

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # nano_magnetic
+=================
+
 Numerical modeling the properties of dense clusters of magnetic nanoparticles.
+
+## Source structure
+
+* `src/nano_magnetic` contains whole the implementation into `*.h` and `*.cc` files and the example into `main.cc`;
+* `src/test` contains Google C++ Testing Framework tests.
+
+## Build
+
+Use CMake:
+
+```
+> mkdir build
+> cd build
+> cmake ..
+> make
+> ./nano_magnetic
+...
+```
+
+## Tests
+
+For testing purposes this project uses Google C++ Testing Framework. To enable tests you should define `ENABLE_TESTS=ON` variable:
+
+```
+> cmake -DENABLE_TESTS=ON ..
+> make
+> ./nano_magnetic_test
+...
+```
---
+++ src/nano_magnetic/main.cc
@@ -0,0 +1,102 @@
+//
+// The MIT License (MIT)
+//
+// Copyright (c) 2016 Maxim Fedorenko <varlllog@gmail.com>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+//  main.cc
+//
+//  Created by Maxim Fedorenko <varlllog@gmail.com> on 12/Feb/2016.
+//
+
+#include <cmath>
+#include <cstdlib>
+
+#include <random>
+
+#include "nano_magnetic/material.h"
+#include "nano_magnetic/particle.h"
+
+int main( int argc, char **argv ) {
+    namespace nm = nano_magnetic;
+
+
+    constexpr auto kAnisotropy = 4000.;
+    constexpr auto kSaturation = 800.;
+    constexpr auto kRadius = 20.e-7;
+
+    auto material = nm::Material(kAnisotropy, kSaturation, kRadius);
+
+    auto particle = nm::Particle(material);
+
+
+    constexpr auto kGyromagneticRatio = 1.76e+7;
+
+
+    std::mt19937 mt_generator;
+    mt_generator.seed( 42 );
+
+    //std::random_device rd;
+    //mt_generator.seed( rd() );
+
+    std::uniform_int_distribution<double> dis( 0., 1. );
+
+    auto field = nm::Vector( dis(mt_generator), dis(mt_generator), dis(mt_generator) );
+    field *= 100.;
+
+
+    constexpr auto kDeltaT = 1.e-8;
+    constexpr auto kEpsilon = 1.e-4;
+
+
+    particle.anisotropy = nm::Vector( dis(mt_generator), dis(mt_generator), dis(mt_generator) );
+    particle.anisotropy.normalize();
+
+    particle.magnetic = particle.anisotropy;
+    
+
+    constexpr auto kMaxIterationCount = 50;
+    for(auto i = 0; i < kMaxIterationCount; ++i)
+    {
+        particle.magnetic.normalize();
+
+        const auto kDamping = (20. * particle.material.anisotropy()) / particle.material.saturation();
+
+        auto field_effective_anisotropy = nm::Vector( - kDamping * particle.magnetic.x(),  - kDamping * particle.magnetic.y(), 0);
+
+        auto field_effective = field_effective_anisotropy + field;
+
+        auto field_r = field_effective - field_effective.cross(particle.magnetic) * kDamping;
+
+        auto field_r_norm = field_r.norm();
+
+        field_r.normalize();
+
+        auto dte = (kGyromagneticRatio * field_r_norm * kDeltaT) / ( 1. + std::pow(kDamping, 2.));
+
+        
+
+    }
+
+
+
+    return EXIT_SUCCESS;
+}
+
